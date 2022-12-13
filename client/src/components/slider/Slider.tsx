6e58e58e253bf28e0107aec306b659883c730c22
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-12-13 10:44:09 -0600

    feat(slider): added slider

---
+++ client/src/components/slider/Slider.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Slider.scss";
+
+export const Slider = () => {
+ const data = [
+  "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+ ];
+
+ return (
+  <div className="slider">
+   <div className="container"></div>
+  </div>
+ );
+};
