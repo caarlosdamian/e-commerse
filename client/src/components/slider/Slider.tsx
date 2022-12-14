a44929ab01bcb8441d75ff1c2227f8de77112fce
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-12-14 10:32:59 -0600

    feat(home): added slider

--- client/src/components/slider/Slider.tsx
+++ client/src/components/slider/Slider.tsx
@@ -1,16 +1,42 @@
-import React from "react";
+import { EastOutlined, WestOutlined } from "@mui/icons-material";
+import React, { useState } from "react";
 import "./Slider.scss";
 
 export const Slider = () => {
+ const [currentSlide, setCurrentSlide] = useState(0);
+
  const data = [
   "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
   "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
   "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
  ];
 
+ const prevSlide = () => {
+  setCurrentSlide(currentSlide === 0 ? data.length - 1 : currentSlide - 1);
+ };
+
+ const nextSlide = () => {
+  setCurrentSlide(currentSlide === data.length - 1 ? 0 : currentSlide + 1);
+ };
+ console.log(currentSlide);
  return (
   <div className="slider">
-   <div className="container"></div>
+   <div
+    className="container"
+    style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+   >
+    <img src={data[0]} alt="" />
+    <img src={data[1]} alt="" />
+    <img src={data[2]} alt="" />
+   </div>
+   <div className="icons">
+    <div className="icon" onClick={prevSlide}>
+     <WestOutlined />
+    </div>
+    <div className="icon" onClick={nextSlide}>
+     <EastOutlined />
+    </div>
+   </div>
   </div>
  );
 };
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import { Slider } from "../../components/slider/Slider";
+import "./Home.scss";
 
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+ return (
+  <div className="home">
+   <Slider />
+  </div>
+ );
+};
