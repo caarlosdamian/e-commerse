a793af8efeb3f074752c85921632c719d573b926
caarlosdamian <dapecarlos@gmail.com>
2023-01-01 13:12:54 -0600

    feat(home): completed home page

--- client/src/components/categories/Categories.tsx
+++ client/src/components/categories/Categories.tsx
@@ -11,22 +11,18 @@ export const Categories = () => {
             src="https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600"
             alt=""
           />
-          <button>
-            <Link to="/products/1" className="link">
-              Sale
-            </Link>
-          </button>
+          <Link to="/products/1" className="link">
+            <button>Sale</button>
+          </Link>
         </div>
         <div className="row">
           <img
             src="https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600"
             alt=""
           />
-          <button>
-            <Link to="/products/2" className="link">
-              Woman
-            </Link>
-          </button>
+          <Link to="/products/2" className="link">
+            <button>Woman</button>
+          </Link>
         </div>
       </div>
       <div className="col">
@@ -35,11 +31,9 @@ export const Categories = () => {
             src="https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600"
             alt=""
           />
-          <button>
-            <Link to="/products/1" className="link">
-              Men
-            </Link>
-          </button>
+          <Link to="/products/1" className="link">
+            <button>New Season</button>
+          </Link>
         </div>
       </div>
       <div className="col col-l">
@@ -50,6 +44,11 @@ export const Categories = () => {
                 src="https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600"
                 alt=""
               />
+              <button>
+                <Link to="/products/1" className="link">
+                  Men
+                </Link>
+              </button>
             </div>
           </div>
           <div className="col">
@@ -58,11 +57,9 @@ export const Categories = () => {
                 src="https://images.pexels.com/photos/2703202/pexels-photo-2703202.jpeg?auto=compress&cs=tinysrgb&w=1600"
                 alt=""
               />
-              <button>
-                <Link to="/products/1" className="link">
-                  Accessories
-                </Link>
-              </button>
+              <Link to="/products/1" className="link">
+                <button>Accessories</button>
+              </Link>
             </div>
           </div>
         </div>
@@ -70,12 +67,10 @@ export const Categories = () => {
           <img
             src="https://images.pexels.com/photos/1159670/pexels-photo-1159670.jpeg?auto=compress&cs=tinysrgb&w=1600"
             alt=""
-          />{" "}
-          <button>
-            <Link to="/products/1" className="link">
-              Shoes
-            </Link>
-          </button>
+          />
+          <Link to="/products/1" className="link">
+            <button>Shoes</button>
+          </Link>
         </div>
       </div>
     </div>
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Categories } from "../../components/categories/Categories";
+import { Contact } from "../../components/contact/Contact";
 import { FeaturedProducts } from "../../components/featuredProducts/FeaturedProducts";
 import { Slider } from "../../components/slider/Slider";
 import "./Home.scss";
@@ -11,6 +12,7 @@ export const Home = () => {
    <FeaturedProducts type='trending' />
    <Categories/>
    <FeaturedProducts type='featured' />
+   <Contact/>
   </div>
  );
 };
