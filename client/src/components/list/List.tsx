fd8790f1a48dff8f5b7bd0c77f4122faa98ed799
caarlosdamian <dapecarlos@gmail.com>
2023-01-02 15:26:14 -0600

    feat(categories): added fetch functionality

--- client/src/components/list/List.tsx
+++ client/src/components/list/List.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useFetch } from "../../hooks/useFetch";
 import { Card } from "../card/Card";
 import "./List.scss";
 
@@ -6,51 +7,19 @@ interface ListProps {
   catId: number;
   maxPrice: number;
   sort: string | null;
+  subCats: never[] | string[];
 }
 
-export const List = ({catId,maxPrice,sort}:ListProps) => {
-  const data = [
-    {
-      id: 1,
-      img2: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Product 1",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Product 1",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 3,
-      img2: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Product 1",
-      isNew: false,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Product 1",
-      isNew: false,
-      oldPrice: 19,
-      price: 12,
-    },
-  ];
+export const List = ({ catId, maxPrice, sort, subCats }: ListProps) => {
+  const { data } = useFetch(
+    `/products?populate=*&[filters][categories][id][$eq]=${catId}${subCats
+      .map((item) => `&[filters][sub_categories][id]=${item}`)
+      .join("")}&[filters][price][$lte]=${maxPrice}${sort !== null ? `&sort=price:${sort}`:''}  `
+  );
   return (
     <div className="list">
-      {data.map((item) => (
-        <Card item={item} key={item.id} />
+      {data?.map((item: any) => (
+        <Card item={item.attributes} key={item.id} />
       ))}
     </div>
   );
--- client/src/pages/products/Products.tsx
+++ client/src/pages/products/Products.tsx
@@ -1,30 +1,49 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { List } from "../../components/list/List";
+import { useFetch } from "../../hooks/useFetch";
 import "./Products.scss";
 
 export const Products = () => {
   const catId = parseInt(useParams().id || "");
   const [maxPrice, setMaxPrice] = useState(1000);
+  const [selectedSubCats, setSelectedSubCats] = useState<string[] | never[]>(
+    []
+  );
   const [sort, setSort] = useState<string | null>(null);
+
+  const { data } = useFetch(
+    `/sub-categories?[filters][categories][id][$eq]=${catId}`
+  );
+
+  const handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.checked);
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+    setSelectedSubCats(
+      isChecked
+        ? [...selectedSubCats, value]
+        : selectedSubCats.filter((item) => item !== value)
+    );
+  };
 
   return (
     <div className="products">
       <div className="left">
         <div className="filterItem">
           <h2>Product Categories</h2>
-          <div className="inputItem">
-            <input type="checkbox" name="" id="1" value={1} />
-            <label htmlFor="1">Shoes</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" name="" id="2" value={2} />
-            <label htmlFor="1">Skirts</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" name="" id="3" value={3} />
-            <label htmlFor="1">Coats</label>
-          </div>
+          {data.map((item: any) => (
+            <div className="inputItem" key={item.id}>
+              <input
+                type="checkbox"
+                name=""
+                id={item.id}
+                value={item.id}
+                onChange={handlechange}
+              />
+              <label htmlFor={item.id}>{item.attributes.title}</label>
+            </div>
+          ))}
         </div>
         <div className="filterItem">
           <h2>Filter by price</h2>
@@ -71,7 +90,12 @@ export const Products = () => {
           src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
           alt=""
         />
-        <List catId={catId} maxPrice={maxPrice} sort={sort} />
+        <List
+          catId={catId}
+          maxPrice={maxPrice}
+          sort={sort}
+          subCats={selectedSubCats}
+        />
       </div>
     </div>
   );
